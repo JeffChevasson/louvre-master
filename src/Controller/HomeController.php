91587997800fc614610b56f70031a4ef84dac066
Jeff Chevasson <jchevasson@gmail.com>
2019-06-28 10:51:19 +0200

    Revert "Merge pull request #2 from JeffChevasson/visit-form"
    This reverts commit a0ef4f58175d89a975acd7f426cd0e25c6b71b6e, reversing
    changes made to b046d41221803f553d45ecce5ec432e40b75b204.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,152 +1,84 @@
 <?php
 namespace App\Controller;
 
-
-
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
-use App\Form\VisitType;
-use App\Entity\Visit;
-
-
-
 
 
 class HomeController extends AbstractController
 {
     /**
-     * @var Environment
-     */
-    private $twig;
-
-    public function __construct(Environment $twig)
-    {
-        $this->twig = $twig;
-    }
-
-
-    /**
-     * page 1 Accueil
+     * page 1 presentation
      *
-     * @Route("/", name="home", methods={"GET"})
-     * @return Response
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
+     * @Route("/", name="homepage"]
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction(): Response
+    public function indexAction()
     {
-
-            return new Response($this->twig->render('frontend/index.html.twig'));
-
+        return $this->render('frontend/index.html.twig');
     }
 
     /**
-     * page 2 choix des billets (entité Visit)
+     * page 2 choix des billets
      *
-     * @Route("/billets", name="billets", methods={"GET" , "POST"})
+     * @Route("/order", name="billets")
      * @param Request $request
-     * @return Response
      */
-    public function orderAction(Request $request): Response
+    public function orderAction(Request $request)
     {
-        //On crée un nouvel objet Visit
-        $visit = new visit;
-        //On appelle le formulaire VisitType
-        $formBuilder = $this->get('form.factory')->createBuilder(VisitType::class, $visit);
-        //A partir du formulaire, on le génère
-        $form = $formBuilder->getForm();
-        //Si la requête est en POST
-        if($request->isMethod('POST'))
-        {
-            $visit->setInvoicedate(new \DateTime());
-            //On fait le lien requête->formulaire
-            // Désormais, la variable $visit contient les valeurs entrées par le visiteur
-            $form->handleRequest($request);
-            //On vérifie que les données entrées sont valides
-            if($form->isValid())
-            {
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($visit);
-                $em->flush();
-                $request->getSession()->get('visit');
-                //On redirige l'acheteur vers la page 3 - identification des visiteurs
-                return $this->redirectToRoute ('identification');
-            }
-        }
-        //On est en GET. On affiche le formulaire
-        return $this->render('frontend/tickets.html.twig', array('form'=>$form->createView()));
-        }
-
-        /**return new Response($this->twig->render('frontend/tickets.html.twig')); */
-
+        $visit = $request->getSession()->get('visit');
+        //
+    }
 
     /**
-     * page 3 identification des visiteurs (entité Identify)
+     * page 3 identification des visiteurs
      *
      * @Route("/identification", name="identification")
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
      */
-    public function identifyAction(): Response
+    public function identifyAction()
     {
-        return new Response($this->twig->render('frontend/identify.html.twig'));
+        //
     }
 
     /**
-     * page 4 coordonnées de l'acheteur (entité Customer)
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
+     * page 4 coordonnées de l'acheteur
+     *
      * @Route("/customer", name="customer")
      */
-    public function customerAction(): Response
+    public function customerAction()
     {
-        return new Response($this->twig->render('frontend/customer.html.twig'));
+        //
     }
 
     /**
      * page 5 paiement
      *
      * @Route("/pay", name="payment")
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
      */
-    public function payAction(): Response
+    public function payAction()
     {
-        return new Response($this->twig->render('frontend/payment.html.twig'));
+        //
     }
 
     /**
      * page 6 confirmation
      *
-     *@Route("/confirmation", name="paymentconfirmation")
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
+     *@Route("/confirmation", name="paymentconfirmation"
      */
-    public function confirmationAction(): Response
+    public function confirmationAction()
     {
-        return new Response($this->twig->render('frontend/paymentconfirmation.html.twig'));
+        //
     }
 
     /**
      * page 7 contact
      *
-     * @Route("/contact", name="contact")
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
+     * @Route("/contact", name="contact"
      */
-    public function contactAction(): Response
+    public function contactAction()
     {
-        return new Response($this->twig->render('frontend/contact.html.twig'));
+        //
     }
 
 }
